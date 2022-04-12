c1df1a55e145aba0e5d7cc8c977605d9e1525eaf
kinho <kinhong99@gmail.com>
2022-04-12 15:27:05 -0400

    reactivity module fundamentals on SFC

---
+++ src/components/ClickCounter.vue
@@ -0,0 +1,22 @@
+<script>
+import {reactive} from 'vue'
+export default {
+	setup() {
+		const state = reactive({count : 0});
+
+		return{
+			state,
+		}
+	}
+}
+</script>
+
+<template>
+	<div>
+		<button>Click {{state.count}}</button>		
+	</div>	
+</template>
+
+<style>
+	
+</style>
