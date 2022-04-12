8c4e9e96e06e9173b8e16e22528ff7c26b72aadd
kinho <kinhong99@gmail.com>
2022-04-12 17:15:35 -0400

    using computed properties to track reactive dependencies and aliviate template bloating

--- src/components/ClickCounter.vue
+++ src/components/ClickCounter.vue
@@ -1,10 +1,14 @@
 <script setup>
-import {reactive, ref} from 'vue'
+import {reactive, ref, computed} from 'vue'
 
 const  state = reactive({
 	counterState : {count : 0},
-	fruits : ["banana", "apple", "lime"]
+	fruits : []
 	});
+// NOTE : the reactive object is not the same as the proxy. ex) state === {} will be false
+// However calling reactive on the object will be the same as proxy. ex) reactive({}) === state will be true
+
+// NOTE : Similar as with the options API, we must return an object which means that state can only be object based and cannot be primitive based.
 
 // An alternative to use primitive types is to use ref, but more importantly it allows us to create a value which we can pass and not lose reactivity
 // which is not the case with reactive
@@ -18,17 +22,24 @@ function incrementCount(){
 
 	state.fruits.push("newFruit" + state.counterState.count); //testing deep reactivity
 }
+function decrementCount(){
+	if(state.counterState.count > 0){ state.counterState.count--; state.fruits.pop();}
+	
+}
 
-// NOTE : the reactive object is not the same as the proxy. ex) state === {} will be false
-// However calling reactive on the object will be the same as proxy. ex) reactive({}) === state will be true
+// Computed properties using the Composition API as follows
+const fruitsMessage = computed(() =>{
+	return state.fruits.length > 0 ? "The fruit market has been replenished" : "No fruits available"
+})
 
-// NOTE : Similar as with the options API, we must return an object which means that state can only be object based and cannot be primitive based.
 </script>
 
 <template>
 	<div>
-		<button @click="incrementCount">Click {{state.counterState.count}}</button>
+		<button @click="incrementCount"> Add more fruits</button>
+		<button @click="decrementCount"> Remove fruit</button>
 		<button @click="num++">Click ref: {{num}}</button>		
+		<p>{{fruitsMessage}} : {{state.fruits.length}}</p>
 		<li v-for="fruit in state.fruits" :key="fruit">
 			<ul>
 				{{fruit}}
