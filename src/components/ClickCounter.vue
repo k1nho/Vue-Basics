d07b0eff3b1e94a09dfa5fda902223fa8056f3ce
kinho <kinhong99@gmail.com>
2022-04-12 16:32:09 -0400

    testing nested data with deep reactivity

--- src/components/ClickCounter.vue
+++ src/components/ClickCounter.vue
@@ -1,26 +1,28 @@
-<script>
+<script setup>
 import {reactive} from 'vue'
-export default {
-	setup() {
-		const state = reactive({count : 0});
 
-		// state mutation
-		const incrementCount = function(){
-			state.count++;
-		}
+const  state = reactive({
+	counterState : {count : 0},
+	fruits : ["banana", "apple", "lime"]
+	});
 
-		return{
-			state,
-			incrementCount
-		}
-	}
+function incrementCount(){
+	state.counterState.count++;
+	// DOM UPDATE does not happen synchronously but it is buffered until the next tick in the cycle update
+	// we can use nextTick(()=>{}) to access the updated dom 
 
+	state.fruits.push("newFruit" + state.counterState.count); //tesrting deep reactivity
 }
 </script>
 
 <template>
 	<div>
-		<button>Click {{state.count}}</button>		
+		<button @click="incrementCount">Click {{state.counterState.count}}</button>		
+		<li v-for="fruit in state.fruits" :key="fruit">
+			<ul>
+				{{fruit}}
+			</ul>
+		</li>
 	</div>	
 </template>
 
