f8c46a330feb882c1e7575aba1a9ff7d801da57a
kinho <kinhong99@gmail.com>
2022-04-12 16:41:48 -0400

    reactive API limitations

--- src/components/ClickCounter.vue
+++ src/components/ClickCounter.vue
@@ -11,8 +11,12 @@ function incrementCount(){
 	// DOM UPDATE does not happen synchronously but it is buffered until the next tick in the cycle update
 	// we can use nextTick(()=>{}) to access the updated dom 
 
-	state.fruits.push("newFruit" + state.counterState.count); //tesrting deep reactivity
+	state.fruits.push("newFruit" + state.counterState.count); //testing deep reactivity
 }
+
+// NOTE : the reactive object is not the same as the proxy. ex) state === {} will be false
+// However calling reactive on the object will be the same as proxy. ex) reactive({}) === state will be true
+
 </script>
 
 <template>
