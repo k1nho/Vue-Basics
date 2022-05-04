4e8ac9b67f312a93b48e3dfcb0de23b37785ed92
kinho <kinhong99@gmail.com>
2022-05-04 16:36:38 -0400

    Getting native DOM element from the event using Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import ClickCounter from './components/ClickCounter.vue'
+import ClickCounter from "./components/ClickCounter.vue";
+
+// We can get the native DOM  element that call the method with e
+function printCount(e) {
+  if (e) {
+    console.log(e.target.name);
+  }
+}
 </script>
 
 <template>
   <main>
     <div>
-      <h1>How to use text interpolation to load Vue instance's data?</h1>
-      <p>We can use text interpolation in Vue by using the mustache syntax "{ { } }"</p> 
-    </div> 
+      <h1 class="title">
+        How to use text interpolation to load Vue instance's data?
+      </h1>
+      <p>
+        We can use text interpolation in Vue by using the mustache syntax "{ { }
+        }"
+      </p>
+    </div>
+    <hr />
     <div>
-      <h1>How to allow reactivity by binding attributes to Vue instance data ? </h1>
-      <p>Idea here is to use the ":" syntax before the attribute this signifies v-bind directive which binds the data to the attribute.</p>
-      <p>We can use the property button to ilustrate the example. The idea here is we want to disable the button dynamically so we use Vue's instance data that resolves the attribute value.</p>
+      <h1 class="title">
+        How to allow reactivity by binding attributes to Vue instance data ?
+      </h1>
+      <p>
+        Idea here is to use the ":" syntax before the attribute this signifies
+        v-bind directive which binds the data to the attribute.
+      </p>
+      <p>
+        We can use the property button to ilustrate the example. The idea here
+        is we want to disable the button dynamically so we use Vue's instance
+        data that resolves the attribute value.
+      </p>
+      <hr />
       <div>
-        <ClickCounter/>
+        <h1 class="title">Listening to events in Vue</h1>
+        <p>
+          We can use the v-on directive (@ for shorthand) to listen to dom
+          events.
+        </p>
+        <p>
+          In addition, we can do it inline or by invoking a method name. It is
+          important to notice that we have access to the native DOM element that
+          triggers the event as a parameter to the method.
+        </p>
+        <div>
+          <button name="btn-kin" @click="printCount">Console Name</button>
+        </div>
+      </div>
+      <div>
+        <ClickCounter />
       </div>
     </div>
   </main>
 </template>
 
 <style>
-@import './assets/base.css';
+@import "./assets/base.css";
 
 #app {
   max-width: 1280px;
@@ -80,6 +116,9 @@ a,
 
   .logo {
     margin: 0 2rem 0 0;
+  }
+  .title {
+    font-weight: bold;
   }
 }
 </style>
