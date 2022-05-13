499685e357ca9d068b13e0b2e03dfacbee9bd2fc
kinho <kinhong99@gmail.com>
2022-05-13 12:47:46 -0400

    basic radio component

---
+++ src/components/InputRadio.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+
+const binary = ref("one");
+</script>
+
+<template>
+  <h2>Basic Radio Example</h2>
+  <input type="radio" id="0" value="zero" v-model="binary" />
+  <label for="0" style="margin-right: 10px">Zero</label>
+  <input type="radio" id="1" value="one" v-model="binary" />
+  <label for="1">One</label>
+  <p>Binary is: {{ binary }}</p>
+</template>
