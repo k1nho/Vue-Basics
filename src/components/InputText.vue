95c4213d627d6b8e00cbaa077d1b7fb66ab928a5
kinho <kinhong99@gmail.com>
2022-05-13 12:48:00 -0400

    basic textbox component

---
+++ src/components/InputText.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+
+const userMsg = ref("");
+</script>
+
+<template>
+  <div>
+    <h2>Basic Input Example</h2>
+    <input v-model="userMsg" placeholder="Write Message and See Change" />
+    <p>Message is: {{ userMsg }}</p>
+  </div>
+</template>
