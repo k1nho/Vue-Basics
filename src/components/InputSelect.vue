9a0722c471c7f6538f8918e99e6e9b0c43d47787
kinho <kinhong99@gmail.com>
2022-05-13 12:48:13 -0400

    basic select component

---
+++ src/components/InputSelect.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+const selectedPokemon = ref("Infernape");
+const options = ref([
+  { text: "Infernape", value: "Infernape" },
+  { text: "Floatzel", value: "Floatzel" },
+  { text: "Torterra", value: "Torterra" },
+]);
+</script>
+
+<template>
+  <div>
+    <h2>Basic Select Example</h2>
+    <select v-model="selectedPokemon">
+      <option v-for="pokemon in options" :value="pokemon.value">
+        {{ pokemon.text }}
+      </option>
+    </select>
+    <p>I choose you! {{ selectedPokemon }}</p>
+  </div>
+</template>
