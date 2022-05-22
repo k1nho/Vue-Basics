3b5c68bf17f17d74273320bcbaee46f364110875
kinho <kinhong99@gmail.com>
2022-05-21 21:48:07 -0400

    basics of lifecycle hooks

---
+++ src/components/LifecycleHooks.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  console.log("LifecyleHooks.vue was mounted!");
+});
+</script>
+
+<template>
+  <div>
+    Each component instance has a series of steps it follows to initialize and
+    run. Along the way it runs functions called lifecycle hooks which give us
+    the opportunity to add code to certain parts of the process.
+  </div>
+</template>
